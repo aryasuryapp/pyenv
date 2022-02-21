6b1f73fc149a83dcc62a8c7dcfaedee7a0b12084
arya.surya <arya.surya@mekari.com>
2022-02-22 02:04:54 +0700

    modify main.py

--- main.py
+++ main.py
@@ -15,17 +15,21 @@ class MainWindow(QMainWindow):
         # Set the central widget of the Window.
         self.setCentralWidget(button)
 
-# You need one (and only one) QApplication instance per application.
-# Pass in sys.argv to allow command line arguments for your app.
-# If you know you won't use command line arguments QApplication([]) works too.
-app = QApplication(sys.argv)
+def main():
+    # You need one (and only one) QApplication instance per application.
+    # Pass in sys.argv to allow command line arguments for your app.
+    # If you know you won't use command line arguments QApplication([]) works too.
+    app = QApplication(sys.argv)
 
-# Create Main Window, which will be our window.
-window = MainWindow()
-window.show() # IMPORTANT!!!!! Windows are hidden by default.
+    # Create Main Window, which will be our window.
+    window = MainWindow()
+    window.show() # IMPORTANT!!!!! Windows are hidden by default.
+    
+    # Start the event loop.
+    sys.exit(app.exec())
 
-# Start the event loop.
-app.exec()
+    # Your application won't reach here until you exit and the event
+    # loop has stopped.
 
-# Your application won't reach here until you exit and the event
-# loop has stopped.
+if __name__ == '__main__':
+    main()
